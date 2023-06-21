d0d1c24e5a3b2ca0c0a82fe11dabe169fb77b97e
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2023-06-21 19:45:15 +0100

    started branch for PMC implementation

--- src/AST.elm
+++ src/AST.elm
@@ -10,38 +10,45 @@ import Maybe
 type alias Name
     = String
 
--- * expressions
-type Expr
-    = App Expr (List Expr)    -- non-empty list of arguments
-    | Lam (List Name) Expr    -- non-empty list of variables
+type alias Tag
+    = String
+      
+-- * expressions 
+type Expr 
+    = App Expr Expr
+    | Lam (Maybe Name) Matching  
+      -- named lambda if it was defined in a binding
     | Var Name
     | Number Int
-    | Boolean Bool
-    | ListLit (List Expr)           -- [1,2,3]
-    | TupleLit (List Expr)          -- (1,2,3)
-    | InfixOp Name Expr Expr        --  operators +, * etc
+    | Cons Tag (List Expr)
+    | ListLit (List Expr)                         -- [1,2,3]
+    | TupleLit (List Expr)                        -- (1,2,3)
+    | InfixOp Name Expr Expr                      --  primitive operators +, * etc
     | IfThenElse Expr Expr Expr
-    | Fail String                 -- runtime errors
-    | Eval Expr                   -- evaluation context
+    | Error                                       -- runtime errors
+      
+-- * matchings
+type Matching 
+    = Return Expr Info              -- matching succedeed
+    | Fail                          -- matching failed
+    | Match Pattern Matching        -- match a pattern
+    | Arg Expr Matching             -- argument supply
+    | Alt Matching Matching         -- alternative
 
+      
 -- * declarations      
 type Decl
     = TypeSig Name Type
-    | Equation Name (List Pattern) Expr 
+    | Equation Name Matching
 
 -- * patterns      
 type Pattern
     = VarP Name
     | BangP Name             -- bang pattern (for strict evaluation)
-    | BooleanP Bool
     | NumberP Int
     | ListP (List Pattern)
-    | ConsP Pattern Pattern
     | TupleP (List Pattern)
-
--- a single alternative
-type alias Alt
-    = (List Pattern, Expr)
+    | ConsP Tag (List Pattern)
 
 
 -- * programs
@@ -50,7 +57,7 @@ type Program
 
 -- * bindings
 type alias Bind
-    = { name: Name, typeSig: Maybe Type, alts: List Alt }
+    = { name: Name, typeSig: Maybe Type, expr: Expr }
 
       
 -- * types
@@ -66,70 +73,91 @@ type Type
 -- term substitutions
 type alias Subst
     = Dict Name Expr
-
     
--- information line associated with a reduction
-type Info
-    = Prim String   -- primitive operation
-    | Rewrite Decl  -- rewrite rule 
-
+-- information associated with a reduction
+type alias Info
+    = String
       
 declName : Decl -> Name
 declName decl
     = case decl of
           TypeSig name _ -> name
-          Equation name _ _ -> name
+          Equation name _ -> name
 
-                               
+
+{-
 -- remove evaluation context marker
-uneval : Expr -> Expr
-uneval expr =
+removeCtx : Expr -> Expr
+removeCtx expr =
     case expr of
         App e1 args ->
-            App (uneval e1) (List.map uneval args)
+            App (removeCtx e1) (List.map removeCtx args)
 
-        Lam x e1 ->
-            Lam x (uneval e1)
+        Lam m ->
+            Lam (removeMatchCtx m)
 
         ListLit es ->
-            ListLit (List.map uneval es)
+            ListLit (List.map removeCtx es)
 
         TupleLit es ->
-            TupleLit (List.map uneval es)
+            TupleLit (List.map removeCtx es)
 
         InfixOp op e1 e2 ->
-            InfixOp op (uneval e1) (uneval e2)
+            InfixOp op (removeCtx e1) (removeCtx e2)
 
         IfThenElse e1 e2 e3 ->
-            IfThenElse (uneval e1) (uneval e2) (uneval e3)
+            IfThenElse (removeCtx e1) (removeCtx e2) (removeCtx e3)
                 
-        Eval e ->
+        Ctx e ->
             e
 
-        _ ->
-            expr
+        Var x ->           
+            Var x        -- NB: these have a different type than the lhs
+                         -- hence the duplication
+        Number n ->
+            Number n
 
--- perform variable substitution
+        Boolean b ->
+            Boolean b
+
+        Error  ->
+            Error 
+          
+
+removeMatchCtx : Matching -> Matching
+removeMatchCtx m
+    = case m of
+          Return expr info ->
+              Return (removeCtx expr) info
+
+          Fail  ->
+              Fail 
+
+          Match patt m1 ->
+              Match patt (removeMatchCtx m1)
+                  
+          Arg expr m1 ->
+              Arg (removeCtx expr) (removeMatchCtx m1)
+
+          Alt m1 m2 ->
+              Alt (removeMatchCtx m1) (removeMatchCtx m2)
+-}
+                
+               
+-- apply substitution to an expression
 applySubst : Subst -> Expr -> Expr
 applySubst s e
     = case e of
-          Number _ ->  e
-                       
-          Boolean _ -> e
-                       
           Var x ->
               case Dict.get x s of
                   Nothing -> e
                   Just e1 -> e1
                              
-          Lam xs e1 ->
-              let
-                  s1 = List.foldr Dict.remove s xs
-              in
-                  Lam xs (applySubst s1 e1)
+          Lam optname m ->
+              Lam optname (applyMatchSubst s m)
                       
-          App e1 args ->
-              App (applySubst s e1) <| List.map (applySubst s) args
+          App e1 e2 ->
+              App (applySubst s e1)(applySubst s e2)
                   
           InfixOp op e1 e2 ->
               InfixOp op (applySubst s e1) (applySubst s e2)
@@ -140,15 +168,45 @@ applySubst s e
           TupleLit l ->
               TupleLit (List.map (applySubst s) l)
 
+          Cons c args ->
+              Cons c (List.map (applySubst s) args)
+
           IfThenElse e1 e2 e3 ->
               IfThenElse (applySubst s e1) (applySubst s e2) (applySubst s e3)
 
-          Fail _ -> e
+          _ ->
+              e
 
-          Eval e1 ->
-              Eval (applySubst s e1)
+{-                   
+          Ctx e1 ->
+              Ctx (applySubst s e1)
+-}
 
                   
+applyMatchSubst : Subst -> Matching -> Matching
+applyMatchSubst s m
+    = case m of
+          Return e info ->
+              Return (applySubst s e) info
+
+          Fail  ->
+              Fail 
+
+          Match p m1 ->
+              let
+                  s1 = List.foldr Dict.remove s (patternVars p)
+              in 
+                  Match p (applyMatchSubst s1 m1)
+
+          Arg e m1 ->
+              Arg (applySubst s e) (applyMatchSubst s m1)
+
+          Alt m1 m2 ->
+              Alt (applyMatchSubst s m1) (applyMatchSubst s m2)
+              
+                  
+
+-- list variables bound by a pattern
 patternVars : Pattern -> List Name
 patternVars patt 
     = case patt of
@@ -160,10 +218,39 @@ patternVars patt
               List.concatMap patternVars ps
           TupleP ps ->
               List.concatMap patternVars ps
-          ConsP p1 p2 ->
-              patternVars p1 ++ patternVars p2
+          ConsP tag ps ->
+              List.concatMap patternVars ps
           _ ->
               []
                       
 
 
+-- * compute the arity of a matching
+matchingArity : Matching  -> Int
+matchingArity m
+    = case m of
+          Return _ _ ->
+              0
+
+          Fail ->
+              0
+
+          Match _ m1 ->
+              1 + matchingArity m1
+
+          Arg _ m1 ->
+              max 0 (matchingArity m1 - 1)
+                        
+          Alt m1 m2 ->
+              max (matchingArity m1) (matchingArity m2)
+                  -- both arities should be equal
+
+isOperator : Name -> Bool
+isOperator = String.all operatorChar 
+                  
+operatorChar : Char -> Bool
+operatorChar c =
+    c=='!' || c=='+' || c=='*' || c=='-' || c=='>' || c=='<' ||
+        c==':' || c=='=' || c=='&' || c=='|' || c=='.' 
+          
+             
---
+++ src/Machine.elm
@@ -0,0 +1,290 @@
+{- 
+ A Lazy abstract machine based on the PMC calculus
+ Pedro Vasconcelos, 2023
+-}
+module Machine exposing (..)
+
+import AST exposing (Expr(..),
+                     Matching(..),
+                     Pattern(..),
+                     Bind,
+                     Decl(..),
+                     Info,
+                     Name,
+                     Subst)
+import Dict exposing (Dict)
+import Debug
+
+type alias Conf
+    = (Heap, Control, Stack)
+
+type alias Heap 
+    = Dict Name Expr
+
+type Control
+    = E Expr
+    | M Matching ArgStack
+
+type alias ArgStack
+    = List Expr
+      
+type alias Stack
+    = List Cont
+
+type Cont
+    = PushArg Expr
+    | Update Name
+    | MatchEnd
+    | PushAlt ArgStack Matching
+    | PushPat ArgStack Pattern Matching
+      -- continuations for primitive operations
+    | RetPrim1 Name Expr
+    | RetPrim2 Name Int 
+      
+
+isWhnf : Expr -> Bool
+isWhnf expr =
+    case expr of
+        Lam _ m ->
+            AST.matchingArity m > 0
+        Number _ ->
+            True
+        ListLit _ ->
+            True
+        TupleLit _ ->
+            True
+        Cons _ _ ->
+            True
+        _ ->
+            False
+
+
+getHeap : Conf -> Heap
+getHeap (heap,_,_) = heap
+
+getControl : Conf -> Control
+getControl (_, control, _) = control
+
+getStack : Conf -> Stack
+getStack (_, _, stack) = stack           
+
+last : List a -> Maybe a
+last xs = case xs of
+              [] -> Nothing
+              [x] -> Just x
+              (x::xs1) -> last xs1
+                         
+--                
+-- a single step transition of the machine
+--
+transition : Conf -> Maybe Conf
+transition conf
+    = case conf of
+          (heap,  E (App e1 e2), stack) ->
+              Just (heap, E e1, PushArg e2::stack)
+                  
+          (heap, E (Lam optname m), stack) ->
+              if AST.matchingArity m == 0 then
+                  Just (heap, M m [], MatchEnd::stack)
+              else
+                  case stack of
+                      PushArg e1::rest ->
+                          Just (heap, E (Lam Nothing (Arg e1 m)), rest)
+                      Update y::rest ->
+                          let
+                              heap1 = Dict.insert y (Lam optname m) heap
+                          in 
+                              Just (heap1, E (Lam optname m), rest)
+                      _ ->
+                          Nothing
+
+          (heap, E (Var y), stack) ->
+              case Dict.get y heap of
+                  Just expr ->
+                      let
+                          heap1 = Dict.remove y heap
+                      in
+                          Just (heap1, E expr, Update y::stack)
+                  _ ->
+                      Nothing
+
+          -- primitive operations
+          (heap, E (InfixOp op e1 e2), stack) ->
+              Just (heap, E e1, (RetPrim1 op e2)::stack)
+                  
+          (heap, E (Number v1), (RetPrim1 op e2)::stack) ->
+              Just (heap, E e2, (RetPrim2 op v1)::stack)
+
+          (heap, E (Number v2), (RetPrim2 op v1)::stack) ->
+              case applyPrimitive op v1 v2 of
+                  Just v ->
+                      Just (heap, E (Number v), stack)
+                  _ ->
+                      Nothing
+                  
+          -- update variable
+          (heap, E w, Update y::stack) ->
+              if isWhnf w then
+                  let
+                      heap1 = Dict.insert y w heap
+                  in
+                      Just (heap1, E w, stack)
+              else
+                  Nothing
+
+          -- bind a variable
+          (heap, M (Match (VarP x) m1) (e1::args), stack) ->
+              let
+                  m2 = AST.applyMatchSubst (Dict.singleton x e1) m1
+              in
+                  Just (heap, M m2 args, stack)
+                      
+          -- match a constructor
+          (heap, M (Match p1 m1) (e1::args), stack) ->
+              Just (heap, E e1, (PushPat args p1 m1)::stack)
+                      
+          -- decompose a constructor 
+          (heap, E (Cons c0 es), (PushPat args (ConsP c1 ps) m1)::stack) ->
+              if c0 == c1 && List.length es == List.length ps then
+                  Just (heap, M (matchCons es ps m1) args, stack) 
+              else
+                  Just (heap, M Fail [], stack)
+
+          (heap, E w, (PushPat args (BangP x) m1)::stack) ->
+              let
+                  m2 = AST.applyMatchSubst (Dict.singleton x w) m1
+              in
+                  Just (heap, M m2 args, stack)                          
+
+          (heap, E (Number n), (PushPat args (NumberP k) m1)::stack) ->
+              if n == k then
+                  Just (heap, M m1 args, stack)
+              else
+                  Just (heap, M Fail [], stack)
+          
+                      
+          -- special cases for list patterns                
+          (heap, E (ListLit []), (PushPat args (ConsP ":" [p1,p2]) m1)::stack) ->
+               Just (heap, M Fail [], stack)                    
+                      
+          (heap, E (ListLit es), (PushPat args (ListP ps) m1)::stack) ->
+              if List.length es == List.length ps then
+                  Just (heap, M (matchCons es ps m1) args, stack)
+              else
+                  Just (heap, M Fail [], stack)
+
+          (heap, E (ListLit (e1::es)), (PushPat args (ConsP ":" [p1,p2]) m1)::stack) ->
+              Just (heap,
+                        M (Arg e1
+                               (Match p1
+                                    (Arg (ListLit es) (Match p2 m1)))) args,
+                        stack)
+
+          (heap, E (Cons ":" [e1,e2]), (PushPat args (ListP (p1::ps)) m1)::stack) ->
+              Just (heap, M (Arg e1 (Match p1
+                                    (Arg e2 (Match (ListP ps) m1)))) args,
+                        stack)
+                  
+          -- successful match: return an expression
+          (heap, M (Return expr info) args, MatchEnd::stack) ->
+              Just (heap, E (applyArgs expr args), stack)
+
+          (heap, M (Return expr info) args, (PushAlt _ _)::stack) ->
+              Just (heap, M (Return expr info) args, stack)
+
+          -- failing match
+          (heap, M Fail _, (PushAlt args m)::stack) ->
+              Just (heap, M m args, stack)
+
+          -- deal with alternatives
+          (heap, M (Alt m1 m2) args, stack) ->
+              Just (heap, M m1 args, PushAlt args m2::stack)
+
+          -- deal with arguments
+          (heap, M (Arg e m1) args, stack) ->
+              Just (heap, M m1 (e::args), stack)
+                  
+          _ ->
+              Nothing
+
+matchCons : List Expr -> List Pattern -> Matching -> Matching
+matchCons es ps m
+    = case (es, ps) of
+          (e::es1, p::ps1) ->
+              Arg e (Match p (matchCons es1 ps1 m))
+          _ ->
+              m
+
+applyArgs : Expr -> ArgStack -> Expr
+applyArgs expr args =
+    case args of
+        [] ->
+            expr
+        (arg1::rest) ->
+            applyArgs (App expr arg1) rest
+              
+
+applyPrimitive : Name -> Int -> Int -> Maybe Int
+applyPrimitive op v1 v2
+    = case op of
+          "+" -> Just (v1 + v2)
+          "-" -> Just (v1 - v2)
+          "*" -> Just (v1 * v2)
+          "div" ->
+              if v2 /= 0 then
+                  Just (v1 // v2)
+              else
+                  Nothing
+          _ ->
+              Nothing
+
+
+
+transitions : Conf -> List Conf
+transitions conf
+    = conf :: case transition conf of
+                  Nothing ->
+                      []
+                  Just conf1 ->
+                      transitions conf1
+
+example0 : Conf
+example0 = (Dict.empty, E (InfixOp "+" (Number 1) (Number 2)), [])
+
+example1 : Conf
+example1 =
+    let
+        heap = Dict.singleton "x" (InfixOp "+" (Number 1) (Number 2))
+        stack = []
+    in 
+        (heap, E (Var "x"), stack)
+
+
+example3 : Conf
+example3 =
+   let tl =  Lam (Just "tail")
+             (Match (ConsP ":" [VarP "h", VarP "t"])
+                      (Return (Var "t") "tail"))
+       e = App tl (ListLit [Number 1, Number 2, Number 3])
+   in (Dict.empty, E e, [])
+
+example4 : Conf
+example4 = (Dict.fromList [],M (Match (VarP "h") (Arg (ListLit []) (Match (VarP "t") (Return (Var "t") "tail")))) [Number 1],[MatchEnd])
+
+
+example5 : Conf
+example5 =
+    let
+        heap = Dict.singleton "sum"
+               (Lam (Just "sum")
+                    (Alt (Match (ConsP ":" [VarP "h",VarP "t"])
+                              (Return (InfixOp "+"
+                                           (Var "h")
+                                           (App (Var "sum") (Var "t")))
+                                           "sum-1"))
+                         (Match (ListP [])
+                              (Return (Number 1) "sum-2"))))
+        control = E (App (Var "sum") (ListLit [Number 1,Number 2,Number 3]))
+        stack = []
+    in
+        (heap, control, stack)

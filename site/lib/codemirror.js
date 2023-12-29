8b8968da8aa8ee487f7711ce19e9466a53942d8f
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2023-12-29 22:07:15 +0000

    switch to softtabs in code editor

--- site/lib/codemirror.js
+++ site/lib/codemirror.js
@@ -6973,7 +6973,7 @@ var commands = {
   },
   defaultTab: function (cm) {
     if (cm.somethingSelected()) { cm.indentSelection("add") }
-    else { cm.execCommand("insertTab") }
+    else { cm.execCommand("insertSoftTab") }
   },
   // Swap the two chars left and right of each selection's head.
   // Move cursor behind the two swapped characters afterwards.
